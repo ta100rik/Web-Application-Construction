099b7a56fb044befcd2b3e11698777874e32cb0d
zyad osseyran <zyadosseyran@gmail.com>
2019-06-21 22:57:43 +0200

    edit db settings

--- src/main/java/api/PostgresBaseDao.java
+++ src/main/java/api/PostgresBaseDao.java
@@ -10,7 +10,7 @@ public class PostgresBaseDao {
         Connection con = null;
         try{
 
-            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/worlddb", "postgres", "root");
+            Connection connection = DriverManager.getConnection("jdbc:postgresql://ec2-79-125-4-72.eu-west-1.compute.amazonaws.com:5432/db5blpva23p33c", "jyzecnyhigmoai", "455c0db18f825c96097f9c4dba9eeab57dcc94d2e55f4c36d910822e55169cc8");
             // When this class first attempts to establish a connection, it automatically loads any JDBC 4.0 drivers found within
             // the class path. Note that your application must manually load any JDBC drivers prior to version 4.0.
 //          Class.forName("org.postgresql.Driver");
